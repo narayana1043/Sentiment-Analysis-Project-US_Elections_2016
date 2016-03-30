7838273b155837144d3daa257e8d8dec7d9a562a
keshavsridhar <keshsridhar@gmail.com>
2016-03-30 13:46:33 -0400

    minor update

--- tweet_db.py
+++ tweet_db.py
@@ -19,21 +19,32 @@ access_token = "574211833-xxYFY0RXSDfgfpGCTV4MjLeyaKpfu1YWw8KOJSUP"             
 access_secret="8NwigwWy8hcFaeEDMtQALQCRp1BUYUALruF68sFpR3ucr"                   #access secret
 
 # creating an OAuthHandler Instance
-auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth=OAuthHandler(consumer_key,consumer_secret)
 
 #equpping OAuthHandler with access token
 auth.set_access_token(access_token,access_secret)
 
-api = tweepy.API(auth)
-
 #creating Mongo Client #alternative: client = MongoClient('mongodb://localhost:27017/')
-#client = MongoClient('localhost', 27017)
-client = MongoClient("mongodb://localhost:27017/")
-db = client['tweetDB']
-collections = db['trump']
+client=MongoClient('localhost',27017)
 
-for status in tweepy.Cursor(api.user_timeline,id="realDonaldTrump").items(1):
-    collections.insert_one(status._json)
+#Setting up DB and collection:
+db=client['Electiondb']
+collection=db['Testcollection']
+
+api=tweepy.API(auth)
+
+#Might have to add exception handling to address timing issue
+for status in tweepy.Cursor(api.user_timeline,id='realDonaldTrump').items(1000):
+    collection.insert(status._json)
+    
+for status in tweepy.Cursor(api.user_timeline,id='HillaryClinton').items(1000):
+    collection.insert(status._json)
+    
+for status in tweepy.Cursor(api.user_timeline,id='tedcruz').items(1000):
+    collection.insert(status._json)
+    
+for status in tweepy.Cursor(api.user_timeline,id='SenSanders').items(1000):
+    collection.insert(status._json)
 
 client.close()
 
