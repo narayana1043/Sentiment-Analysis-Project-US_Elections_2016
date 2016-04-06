a39cdc6388d4c8e020fc51308294224ede1fd451
Brahmendra Sravan Kumar Patibandla <pbskumar@live.com>
2016-04-05 22:55:03 -0400

    Removed API keys

--- tweet_db.py
+++ tweet_db.py
@@ -13,10 +13,10 @@ from pymongo import MongoClient
 import json
 
 #linking the api account keys
-consumer_key = "URKEq38uEfEBYJKhtJsulCkT3"                                      #consumer token
-consumer_secret="R7Ura3hXyreftZsFlesQ0eMMUopxRbfvVxBG6qlbzDQH87MjD8"            #consumer secret
-access_token = "574211833-xxYFY0RXSDfgfpGCTV4MjLeyaKpfu1YWw8KOJSUP"             #access token
-access_secret="8NwigwWy8hcFaeEDMtQALQCRp1BUYUALruF68sFpR3ucr"                   #access secret
+consumer_key = ""                                       #consumer token
+consumer_secret=""                                      #consumer secret
+access_token = ""                                       #access token
+access_secret=""                                        #access secret
 
 # creating an OAuthHandler Instance
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
