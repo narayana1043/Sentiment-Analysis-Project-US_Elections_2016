5bd4c08ec78a58fb294d4259243a42a6b54ff93d
Brahmendra Sravan Kumar Patibandla <pbskumar@live.com>
2016-05-04 22:22:22 -0400

    Abstraction

--- Candidate_LDA.py
+++ Candidate_LDA.py
@@ -1,48 +1,37 @@
 import re
 from nltk.corpus import stopwords
-from gensim import corpora,models
+from gensim import corpora, models
 from pprint import pprint
 
+
 def ReadData(filename):
-    speech=[]
-    with open(filename,'r') as f:
-        line=f.readline()
+    speech = []
+    # , encoding='utf-8'
+    with open(filename, 'r', encoding='utf-8') as f:
+        line = f.readline()
         speech.append(line)
         for line in f:
-            #c=f.readline()
-           speech.append(line)
+            # c=f.readline()
+            speech.append(line)
     return speech
 
 
 def LDA(candidate):
-    sw=set(stopwords.words("english"))
+    sw = set(stopwords.words("english"))
     sw.add("rt")
 
-
     for i in range(len(candidate)):
-        x=re.findall(r"\w+'?[a-z]",candidate[i])
-        x=set([word.lower() for word in x])
-        candidate[i]=x-sw
-        
-    dictionary=corpora.Dictionary(candidate)
-    corpus=[dictionary.doc2bow(text) for text in candidate]
-    ldamodel = models.ldamodel.LdaModel(corpus, num_topics=10, id2word = dictionary)
+        x = re.findall(r"\w+'?[a-z]", candidate[i])
+        x = set([word.lower() for word in x])
+        candidate[i] = x - sw
+
+    dictionary = corpora.Dictionary(candidate)
+    corpus = [dictionary.doc2bow(text) for text in candidate]
+    ldamodel = models.ldamodel.LdaModel(corpus, num_topics=10, id2word=dictionary, passes=3)
     return ldamodel
-    
-# hillary=ReadData('Hillary Clinton Calls for Universal Preschool.txt')
-# ldahillary=LDA(hillary)
-# pprint(ldahillary.print_topics(num_topics=2))
 
-bernie=ReadData('bernie.txt')
-ldabernie=LDA(bernie)
-pprint(ldabernie.print_topics(num_topics=10))
 
-#
-# trump=ReadData('')
-# ldatrump=LDA(trump)
-#
-# cruz=ReadData('')
-# ldacruz=LDA(cruz)
-#
-# kasich=ReadData('')
-# ldakasich=LDA(kasich)
+file_name = 'Path/to/file'
+speech = ReadData(file_name)
+lda_result = LDA(speech)
+pprint(lda_result.print_topics(num_topics=10))
